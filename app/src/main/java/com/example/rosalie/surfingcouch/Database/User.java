43e15e5f4197b997d326cd3112a974ae725456fc
RosaliePeree <rosalie.peree@etu.univ-lyon1.fr>
2017-12-07 14:35:36 +0100

    adding user and add to the database

--- app/src/main/java/com/example/rosalie/surfingcouch/Database/User.java
+++ app/src/main/java/com/example/rosalie/surfingcouch/Database/User.java
@@ -1,37 +1,85 @@
 package com.example.rosalie.surfingcouch.Database;
 
+import java.util.ArrayList;
+
 /**
  * Created by infer on 05/12/2017.
  */
 
 public class User {
-    private Area comefrom;
+    private String comefrom;
     private String email;
-    private int id;
+    private String gender;
     private int numberOfPoints;
-    private HostingPlace[] places;
-    private Reviews[] reviews;
-    private Trip[] trips;
-    private Rewards[] unlockedRewards;
+    private ArrayList<String>  places;
+    private ArrayList<String> reviews;
+    private ArrayList<String>  trips;
+    private ArrayList<String> unlockedRewards;
     private String username;
+    private ArrayList<String> conversations;
 
-    public User(Area comefrom, String email, int id, int numberOfPoints, HostingPlace[] places, Reviews[] reviews, Trip[] trips, Rewards[] unlockedRewards, String username) {
+    public User() {
+    }
+
+
+    public User(String comefrom, String email, String gender, int numberOfPoints, ArrayList<String> places, ArrayList<String> reviews, ArrayList<String> trips, ArrayList<String> unlockedRewards, String username, ArrayList<String> conversations) {
         this.comefrom = comefrom;
         this.email = email;
-        this.id = id;
+        this.gender = gender;
         this.numberOfPoints = numberOfPoints;
         this.places = places;
         this.reviews = reviews;
         this.trips = trips;
         this.unlockedRewards = unlockedRewards;
         this.username = username;
+        this.conversations = conversations;
     }
 
-    public Area getComefrom() {
+    public ArrayList<String> getConversations() {
+        return conversations;
+    }
+
+    public void setConversations(ArrayList<String> conversations) {
+        this.conversations = conversations;
+    }
+
+    public ArrayList<String> getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(ArrayList<String> places) {
+        this.places = places;
+    }
+
+    public ArrayList<String> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(ArrayList<String> reviews) {
+        this.reviews = reviews;
+    }
+
+    public ArrayList<String> getTrips() {
+        return trips;
+    }
+
+    public void setTrips(ArrayList<String> trips) {
+        this.trips = trips;
+    }
+
+    public ArrayList<String> getUnlockedRewards() {
+        return unlockedRewards;
+    }
+
+    public void setUnlockedRewards(ArrayList<String> unlockedRewards) {
+        this.unlockedRewards = unlockedRewards;
+    }
+
+    public String getComefrom() {
         return comefrom;
     }
 
-    public void setComefrom(Area comefrom) {
+    public void setComefrom(String comefrom) {
         this.comefrom = comefrom;
     }
 
@@ -43,14 +91,6 @@ public class User {
         this.email = email;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public int getNumberOfPoints() {
         return numberOfPoints;
     }
@@ -59,43 +99,19 @@ public class User {
         this.numberOfPoints = numberOfPoints;
     }
 
-    public HostingPlace[] getPlaces() {
-        return places;
-    }
-
-    public void setPlaces(HostingPlace[] places) {
-        this.places = places;
-    }
-
-    public Reviews[] getReviews() {
-        return reviews;
-    }
-
-    public void setReviews(Reviews[] reviews) {
-        this.reviews = reviews;
-    }
-
-    public Trip[] getTrips() {
-        return trips;
-    }
-
-    public void setTrips(Trip[] trips) {
-        this.trips = trips;
-    }
-
-    public Rewards[] getUnlockedRewards() {
-        return unlockedRewards;
-    }
-
-    public void setUnlockedRewards(Rewards[] unlockedRewards) {
-        this.unlockedRewards = unlockedRewards;
-    }
-
     public String getUsername() {
         return username;
     }
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 }
