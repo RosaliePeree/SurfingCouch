99a74e03c2e67065166d5ec5333cfa903c9f6c17
SpooyRainbow <infernoxe42@hotmail.fr>
2017-12-08 13:48:00 +0100

    Changes to USER Class + XML for Adding hosting places

---
+++ app/src/main/java/com/example/rosalie/surfingcouch/AddHostingPlaceActivity.java
@@ -0,0 +1,13 @@
+package com.example.rosalie.surfingcouch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddHostingPlaceActivity extends NavigationDrawerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_hosting_place);
+    }
+}
